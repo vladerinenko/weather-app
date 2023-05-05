a610b7d386da5782b7cd2470cb127e6760b68aee
vladerinenko <vlad.erinenko@gmail.com>
2023-05-05 13:41:27 +0300

    create Navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
+  <div
+      class="flex flex-col min-h-screen font-Roboto bg-weather-primary"
+  >
+    <Navigation />
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import Navigation from "./components/Navigation.vue";
 </script>
 
 <style lang="scss" scoped></style>
